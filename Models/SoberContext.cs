3e897128d6b92d26300d13c413826020721275f9
Travis Sunderland <travis@sunderland.io>
2019-08-20 15:52:14 -0500

    init

---
+++ Models/SoberContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordBot.Models
+{
+    public class SoberContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=soberbot.db");
+        }
+
+        public DbSet<Sobriety> Sobrieties { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using DiscordBot.Services;
+using DiscordBot.Models;
+
+namespace DiscordBot
+{
+    class Program
+    {
+        static void Main()
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        private IConfiguration _config;
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _config = BuildConfig();
+
+            var services = ConfigureServices();
+            services.GetRequiredService<LogService>();
+            await services.GetRequiredService<CommandHandlingService>().InitializeAsync(services);
+            services.GetRequiredService<DatabaseService>().Initialize(new SoberContext());
+
+            await _client.LoginAsync(TokenType.Bot, _config["token"]);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private IServiceProvider ConfigureServices()
+        {
+            return new ServiceCollection()
+                // Base
+                .AddSingleton(_client)
+                .AddSingleton<CommandService>()
+                .AddSingleton<CommandHandlingService>()
+                // Logging
+                .AddLogging()
+                .AddSingleton<LogService>()
+                // Extra
+                .AddSingleton(_config)
+                // Add additional services here...
+                .AddSingleton<DatabaseService>()
+                .BuildServiceProvider();
+        }
+
+        private IConfiguration BuildConfig()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("config.json")
+                .Build();
+        }
+    }
+}
